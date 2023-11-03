0b42c42039dc6a7ce67d570ae75f062e1787a231
binsim <jonasspitzhirn@gmail.com>
2023-11-03 14:45:26 +0100

    Teste online Nachrichten vom ESP

--- rpi-server/src/mqttService.test.ts
+++ rpi-server/src/mqttService.test.ts
@@ -21,4 +21,40 @@ describe('MQTTService', () => {
 
 		expect(service.devices.at(-1)?.id).toBe(deviceID);
 	});
+
+	test('Device online message', () => {
+		const device = service.getDeviceByID(deviceID);
+
+		let isOnline = false;
+
+		device?.onOnlineChanged((value) => (isOnline = value));
+		(service as any).onMessageArrived(`/${deviceID}/online`, 'online');
+
+		expect(isOnline).toBe(true);
+		expect(device?.isOnline).toBe(true);
+	});
+	test('Device offline message', () => {
+		const device = service.getDeviceByID(deviceID);
+
+		let isOnline = true;
+
+		device?.onOnlineChanged((value) => (isOnline = value));
+		(service as any).onMessageArrived(`/${deviceID}/online`, 'offline');
+
+		expect(isOnline).toBe(false);
+		expect(device?.isOnline).toBe(false);
+	});
+	test('Wrong online Message', () => {
+		const device = service.getDeviceByID(deviceID);
+		// Execute this to trigger onOnlineChanged for wrong message
+		(service as any).onMessageArrived(`/${deviceID}/online`, 'online');
+
+		let isOnline = true;
+
+		device?.onOnlineChanged((value) => (isOnline = value));
+		(service as any).onMessageArrived(`/${deviceID}/online`, 'Wrong');
+
+		expect(isOnline).toBe(false);
+		expect(device?.isOnline).toBe(false);
+	});
 });
