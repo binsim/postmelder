e96ebc2a1d42dd7c2cf74cc84fd2d2add912efa4
binsim <jonasspitzhirn@gmail.com>
2023-11-12 00:45:07 +0100

    Bugfix

--- rpi-server/public/index.js
+++ rpi-server/public/index.js
@@ -1,6 +1,10 @@
-let notification_username_element = document.querySelector('.notServiceConf #user');
-let notification_password_element = document.querySelector('.notServiceConf #password');
+let notification_username_element = document.querySelector(
+	'.notServiceConf #username'
+);
+let notification_password_element = document.querySelector(
+	'.notServiceConf #password'
+);
 
 notification_username_element.addEventListener('change', () => {
 	notification_password_element.disabled = false;
-})
+});
