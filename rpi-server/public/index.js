49242f9e4c61b95c2e0db0df9348dcfded97b3c9
binsim <jonasspitzhirn@gmail.com>
2023-11-12 00:58:07 +0100

    Setzen des Inputfeldes disabled

--- rpi-server/public/index.js
+++ rpi-server/public/index.js
@@ -4,7 +4,21 @@ let notification_username_element = document.querySelector(
 let notification_password_element = document.querySelector(
 	'.notServiceConf #password'
 );
+let notification_port_checkbox = document.querySelector(
+	'.notServiceConf #port-enabled'
+);
+let notification_port_input = document.querySelector('.notServiceConf #port');
 
 notification_username_element.addEventListener('change', () => {
 	notification_password_element.disabled = false;
+});
+
+notification_port_input.disabled = !notification_port_checkbox.checked;
+notification_port_checkbox.addEventListener('change', () => {
+	console.log(notification_port_checkbox.checked);
+	notification_port_input.disabled = !notification_port_checkbox.checked;
+
+	if (!notification_port_checkbox.checked) {
+		notification_port_input.value = DEFAULT_SMTP_PORT;
+	}
 });
