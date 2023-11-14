a13a0679b5533dc6735ae6400f2fdc1ab3e6cf9d
binsim <jonasspitzhirn@gmail.com>
2023-11-14 11:38:11 +0100

    Löschen nicht benötigten Ordners

--- esp32/code/.pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.d
+++
@@ -1,5 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.o: \
- C:/Users/florian.gr/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
- C:/Users/florian.gr/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
- C:/Users/florian.gr/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- C:/Users/florian.gr/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
