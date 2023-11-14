451d23482eb43ec6f66334eeb59fb5eba9e7bd9f
binsim <jonasspitzhirn@gmail.com>
2023-11-14 11:40:57 +0100

    TabSize auf 4 gesetzt

--- esp-client/src/main.cpp
+++ esp-client/src/main.cpp
@@ -20,184 +20,184 @@ bool ausgeloest = false;
 
 void setup()
 {
-  pinMode(RLedPin, OUTPUT); // Pins initialisieren
-  pinMode(GLedPin, OUTPUT);
-  pinMode(BLedPin, OUTPUT);
+	pinMode(RLedPin, OUTPUT); // Pins initialisieren
+	pinMode(GLedPin, OUTPUT);
+	pinMode(BLedPin, OUTPUT);
 
-  ledcSetup(0, 10, 12); // PWM für blaue LED
-  ledcAttachPin(BLedPin, 0);
+	ledcSetup(0, 10, 12); // PWM für blaue LED
+	ledcAttachPin(BLedPin, 0);
 
-  ledcWrite(0, 100);    // blaue LED blinken
-  Serial.begin(115200); // Serielle USB-Verbindung für Debugging
-  delay(100);
+	ledcWrite(0, 100);	  // blaue LED blinken
+	Serial.begin(115200); // Serielle USB-Verbindung für Debugging
+	delay(100);
 
-  HCSR04.begin(triggerPin, echoPins, echoCount); // Sensor starten
+	HCSR04.begin(triggerPin, echoPins, echoCount); // Sensor starten
 
-  calibrateEcho(); // Sensoren auf Fach kalibrieren
+	calibrateEcho(); // Sensoren auf Fach kalibrieren
 
-  ledcWrite(0, 100); // blaue LED aus
+	ledcWrite(0, 100); // blaue LED aus
 }
 
 // calibrateEcho
 void calibrateEcho()
 {
-  Serial.println("----------------");
-  Serial.println("calibrateEcho()");
+	Serial.println("----------------");
+	Serial.println("calibrateEcho()");
 
-  measureMean(5); // Mittelwert aus 5 Messungen
+	measureMean(5); // Mittelwert aus 5 Messungen
 
-  threshhold1 = meanDistances[0] * threshholdRatio; // mit threshholdRation multiplizieren
-  threshhold2 = meanDistances[1] * threshholdRatio;
+	threshhold1 = meanDistances[0] * threshholdRatio; // mit threshholdRation multiplizieren
+	threshhold2 = meanDistances[1] * threshholdRatio;
 
-  Serial.print("Thresholds: ");
-  Serial.print(threshhold1);
-  Serial.print(", ");
-  Serial.println(threshhold2);
-  Serial.println("----------------");
+	Serial.print("Thresholds: ");
+	Serial.print(threshhold1);
+	Serial.print(", ");
+	Serial.println(threshhold2);
+	Serial.println("----------------");
 }
 
 // measureMean
 void measureMean(int cycles)
 { // liest die Sensoren "cycles" mal aus und bildet den Mittelwert
 
-  Serial.println("----------------");
-  Serial.println("measureMean()");
+	Serial.println("----------------");
+	Serial.println("measureMean()");
 
-  float mean1 = 0;
-  float mean2 = 0;
+	float mean1 = 0;
+	float mean2 = 0;
 
-  for (int i = 0; i < cycles; i++)
-  {
-    distances = HCSR04.measureDistanceCm();
+	for (int i = 0; i < cycles; i++)
+	{
+		distances = HCSR04.measureDistanceCm();
 
-    Serial.print("Messung ");
-    Serial.print(i);
-    Serial.print(": ");
-    Serial.print(distances[0]);
-    Serial.print(", ");
-    Serial.print(distances[1]);
-    Serial.print(", ");
+		Serial.print("Messung ");
+		Serial.print(i);
+		Serial.print(": ");
+		Serial.print(distances[0]);
+		Serial.print(", ");
+		Serial.print(distances[1]);
+		Serial.print(", ");
 
-    mean1 += distances[0];
-    mean2 += distances[1];
+		mean1 += distances[0];
+		mean2 += distances[1];
 
-    Serial.print("means: ");
-    Serial.print(mean1);
-    Serial.print(", ");
-    Serial.println(mean2);
-    delay(100);
-  }
+		Serial.print("means: ");
+		Serial.print(mean1);
+		Serial.print(", ");
+		Serial.println(mean2);
+		delay(100);
+	}
 
-  mean1 /= cycles;
-  mean2 /= cycles;
+	mean1 /= cycles;
+	mean2 /= cycles;
 
-  meanDistances[0] = mean1;
-  meanDistances[1] = mean2;
+	meanDistances[0] = mean1;
+	meanDistances[1] = mean2;
 
-  mean1 = 0;
-  mean2 = 0;
+	mean1 = 0;
+	mean2 = 0;
 
-  Serial.print("Mittelwerte: ");
-  Serial.print(meanDistances[0]);
-  Serial.print(", ");
-  Serial.println(meanDistances[1]);
-  Serial.println("----------------");
+	Serial.print("Mittelwerte: ");
+	Serial.print(meanDistances[0]);
+	Serial.print(", ");
+	Serial.println(meanDistances[1]);
+	Serial.println("----------------");
 }
 
 // belegt
 bool belegt()
 {
-  Serial.println("----------------");
-  Serial.println("belegt()");
+	Serial.println("----------------");
+	Serial.println("belegt()");
 
-  measureMean(2); // Mittelwert aus 2 Messungen
+	measureMean(2); // Mittelwert aus 2 Messungen
 
-  if (meanDistances[0] <= threshhold1 && meanDistances[1] <= threshhold2)
-  { // wenn beide unter Threshhold, dann auslösen
-    Serial.println("BELEGT");
-    Serial.println("----------------");
-    return true;
-  }
-  else
-  {
-    Serial.println("NICHT BELEGT");
-    Serial.println("----------------");
-    return false;
-  }
+	if (meanDistances[0] <= threshhold1 && meanDistances[1] <= threshhold2)
+	{ // wenn beide unter Threshhold, dann auslösen
+		Serial.println("BELEGT");
+		Serial.println("----------------");
+		return true;
+	}
+	else
+	{
+		Serial.println("NICHT BELEGT");
+		Serial.println("----------------");
+		return false;
+	}
 }
 
 // geleert
 bool geleert()
 { // stellt fest, ob das Postfach 1min lang leer war (Überprüfung ca. alle 20s -> 3x)
-  static unsigned long zeit = 0;
-  static int counter = 0;
-  static int cycle = 0;
+	static unsigned long zeit = 0;
+	static int counter = 0;
+	static int cycle = 0;
 
-  Serial.println("----------------");
-  Serial.println("geleert()");
-  Serial.print("cycle ");
-  Serial.println(cycle);
+	Serial.println("----------------");
+	Serial.println("geleert()");
+	Serial.print("cycle ");
+	Serial.println(cycle);
 
-  if (zeit > millis()) // reset bei Overflow von millis()
-  {
-    zeit = millis();
-  }
+	if (zeit > millis()) // reset bei Overflow von millis()
+	{
+		zeit = millis();
+	}
 
-  if (cycle == 0)
-  {
-    zeit = millis(); // Zeit beim ersten Abruf speichern
-    Serial.print("Zeit: ");
-    Serial.println(zeit);
+	if (cycle == 0)
+	{
+		zeit = millis(); // Zeit beim ersten Abruf speichern
+		Serial.print("Zeit: ");
+		Serial.println(zeit);
 
-    cycle++;
-  }
+		cycle++;
+	}
 
-  if (millis() >= (zeit + cycle * 20000))
-  { // ca. alle 20s
-    Serial.println("MESSEN");
+	if (millis() >= (zeit + cycle * 20000))
+	{ // ca. alle 20s
+		Serial.println("MESSEN");
 
-    if (belegt())
-    { // wenn belegt, 1 hochzählen
-      counter++;
-    }
-    cycle++;
-  }
+		if (belegt())
+		{ // wenn belegt, 1 hochzählen
+			counter++;
+		}
+		cycle++;
+	}
 
-  Serial.print("counter: ");
-  Serial.println(counter);
+	Serial.print("counter: ");
+	Serial.println(counter);
 
-  if (counter == 0 && cycle == 3)
-  {            // wenn fertig durchgelaufen und nicht belegt
-    cycle = 0; // von neuem anfangen
-    return true;
-  }
-  else if (cycle == 3)
-  {            // wenn fertig durchgelaufen und belegt
-    cycle = 0; // von neuem anfangen
-    counter = 0;
-    return false;
-  }
-  else
-  {
-    return false;
-  }
+	if (counter == 0 && cycle == 3)
+	{			   // wenn fertig durchgelaufen und nicht belegt
+		cycle = 0; // von neuem anfangen
+		return true;
+	}
+	else if (cycle == 3)
+	{			   // wenn fertig durchgelaufen und belegt
+		cycle = 0; // von neuem anfangen
+		counter = 0;
+		return false;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 // loop
 void loop()
 {
-  if (!ausgeloest)
-  { // wenn ausgeloest, dann grüne LED an und publishen
-    if (belegt())
-    {
-      ausgeloest = true;
-      digitalWrite(GLedPin, HIGH);
-    }
-  }
-  else if (geleert())
-  { // wenn wieder geleert, dann grüne LED aus und publishen
+	if (!ausgeloest)
+	{ // wenn ausgeloest, dann grüne LED an und publishen
+		if (belegt())
+		{
+			ausgeloest = true;
+			digitalWrite(GLedPin, HIGH);
+		}
+	}
+	else if (geleert())
+	{ // wenn wieder geleert, dann grüne LED aus und publishen
 
-    ausgeloest = false;
-    digitalWrite(GLedPin, LOW);
-  }
+		ausgeloest = false;
+		digitalWrite(GLedPin, LOW);
+	}
 }
