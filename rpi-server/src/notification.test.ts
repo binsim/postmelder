bb9ec36ce558792909a068e02b9146731f6f5f95
binsim <jonasspitzhirn@gmail.com>
2023-11-20 21:47:19 +0100

    Hinzufügen der Test für zukünftige Variablen

--- rpi-server/src/notification.test.ts
+++ rpi-server/src/notification.test.ts
@@ -15,4 +15,42 @@ describe('NotificationService', () => {
 			(NotificationService as any).insertVariables(text, { boxNumber: 1 })
 		).toBe('Box 1 got occupied \n\nPlease check 1');
 	});
+	test('Replace weight', () => {
+		let text = 'The weight of the box has changed to {WEIGHT}';
+
+		expect(
+			(NotificationService as any).insertVariables(text, {
+				currentWeight: 10,
+			})
+		).toBe('The weight of the box has changed to 10g');
+	});
+	test('Replace lastEmptied', () => {
+		let text = 'Box hast been emptied last at {LASTEMPTIED}';
+		const time = Date.now().valueOf();
+
+		expect(
+			(NotificationService as any).insertVariables(text, {
+				lastEmptied: time,
+			})
+		).toBe(
+			'Box hast been emptied last at ' + new Date(time).toLocaleString()
+		);
+	});
+	test('Replace history', () => {
+		let text = 'Box fill history:\n{HISTORY}';
+		const time = Date.now().valueOf();
+
+		expect(
+			(NotificationService as any).insertVariables(text, {
+				history: [
+					{ timeStamp: time, weight: 10 },
+					{ timeStamp: time, weight: 20 },
+				],
+			})
+		).toBe(
+			`Box fill history:\n\n${new Date(
+				time
+			).toLocaleString()}: 10g\n${new Date(time).toLocaleString()}: 20g\n`
+		);
+	});
 });
