c297119780783d0db0cd955c9abf4c91561624c4
binsim <64983699+binsim@users.noreply.github.com>
2023-11-23 11:01:20 +0100

    Hinzufügen grundlegendes Logging

---
+++ rpi-server/src/logging.ts
@@ -0,0 +1,22 @@
+import winston, { createLogger } from 'winston';
+
+const enumerateErrorFormat = winston.format((info) => {
+	if (info instanceof Error) {
+		Object.assign(info, { message: info.stack });
+	}
+	return info;
+});
+
+export const logger = createLogger({
+	level: process.env.LOG_LEVEL ?? 'debug',
+	format: winston.format.combine(
+		enumerateErrorFormat(),
+		winston.format.colorize(),
+		winston.format.printf(({ level, message }) => `${level}: ${message}`)
+	),
+	transports: [
+		new winston.transports.Console({
+			stderrLevels: ['error'],
+		}),
+	],
+});
