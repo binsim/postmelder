442973b128e37f7f3fae65b8161a0ecdbb5c5b0d
binsim <64983699+binsim@users.noreply.github.com>
2023-11-30 11:41:18 +0100

    Formatierung des SourceCodes

--- esp-client/src/main.cpp
+++ esp-client/src/main.cpp
@@ -3,23 +3,21 @@
 #include <PubSubClient.h>
 
 #define SSID "Postmelder-Wifi"
-IPAddress mqttServer(10,42,0,1);
+IPAddress mqttServer(10, 42, 0, 1);
 #define MQTTUSER "MQTTBroker"
 #define MQTTPASS "postmelder"
 
 WiFiClient wiFiClient;
 PubSubClient client(wiFiClient);
 
-
-void callback(char* topic, byte* message, unsigned int length);
+void callback(char *topic, byte *message, unsigned int length);
 void reconnect();
 void sendWeight(float weight);
 const String MAC = WiFi.macAddress();
 bool connectedWithNode = false;
 bool isServerOnline = false;
 
-
-void setup() 
+void setup()
 {
 	Serial.begin(115200);
 
@@ -29,73 +27,74 @@ void setup()
 	WiFi.begin(SSID);
 
 	client.setServer(mqttServer, 1883);
-  	client.setCallback(callback);
-
+	client.setCallback(callback);
 }
 
-void loop() 
+void loop()
 {
-	if (WiFi.status() != WL_CONNECTED) 
+	if (WiFi.status() != WL_CONNECTED)
 	{
 		// TODO: Show Output RGB
 	}
-	else 
+	else
 	{
 		reconnect();
 	}
 	client.loop();
 }
-void callback(char* topic, byte* message, unsigned int length)
+void callback(char *topic, byte *message, unsigned int length)
 {
-  	String messageTemp;
+	String messageTemp;
 	String topicStr(topic);
 
 	Serial.print("Message arrived on topic: '");
-  	Serial.print(topic);
-  	Serial.print("' Message: '");
-  
-  	for (int i = 0; i < length; i++) {
-    	Serial.print((char)message[i]);
-    	messageTemp += (char)message[i];
-  	}
-  	Serial.println("'");
+	Serial.print(topic);
+	Serial.print("' Message: '");
+
+	for (int i = 0; i < length; i++)
+	{
+		Serial.print((char)message[i]);
+		messageTemp += (char)message[i];
+	}
+	Serial.println("'");
 
 	if (topicStr == ("/" + MAC))
 	{
 		// TODO: Handle case
 		connectedWithNode = true;
 	}
-	else if(topicStr == "/server/online")
+	else if (topicStr == "/server/online")
 	{
 		// TODO: Let user know
-		if (messageTemp != "connected" && messageTemp != "disconnected") 
+		if (messageTemp != "connected" && messageTemp != "disconnected")
 			return;
 		isServerOnline = messageTemp == "connected";
 	}
 }
-void reconnect() 
+void reconnect()
 {
 
-	if (client.connected()) return;
-	
+	if (client.connected())
+		return;
+
 	// TODO: Get MAC Address as ID
-	if (client.connect(MAC.c_str(), MQTTUSER, MQTTPASS, ("/" + MAC + "/online").c_str(), 1, true, "disconnected")) {
+	if (client.connect(MAC.c_str(), MQTTUSER, MQTTPASS, ("/" + MAC + "/online").c_str(), 1, true, "disconnected"))
+	{
 		client.subscribe(("/" + MAC + "/#").c_str());
 		client.subscribe("/server/online");
 
 		// Sending device now available
 		client.publish("/devices", MAC.c_str());
 		client.publish(("/" + MAC + "/online").c_str(), "connected", true);
-	} 
+	}
 	else
 	{
 		Serial.print("failed, rc=");
-      	Serial.println(client.state());
+		Serial.println(client.state());
 		delay(1000);
 	}
-
 }
 void sendWeight(float weight)
 {
-	client.publish(("/" + MAC + "/currentWeight").c_str(), String(weight, 1).c_str(),true);
+	client.publish(("/" + MAC + "/currentWeight").c_str(), String(weight, 1).c_str(), true);
 }
