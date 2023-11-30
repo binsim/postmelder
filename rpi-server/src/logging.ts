0a83d7d711c67157c90a46505b2d3a5c1a26eb46
binsim <64983699+binsim@users.noreply.github.com>
2023-11-30 12:08:51 +0100

    Logge nicht zur Console beim Testen

--- rpi-server/src/logging.ts
+++ rpi-server/src/logging.ts
@@ -7,6 +7,19 @@ const enumerateErrorFormat = winston.format((info) => {
 	return info;
 });
 
+const transports: winston.transport[] = [
+	new winston.transports.File({
+		filename: `data/${new Date(Date.now()).toDateString()}.log`,
+	}),
+];
+if (process.env.NODE_ENV !== 'test') {
+	transports.push(
+		new winston.transports.Console({
+			stderrLevels: ['error'],
+		})
+	);
+}
+
 export const logger = createLogger({
 	level: process.env.LOG_LEVEL ?? 'debug',
 	format: winston.format.combine(
@@ -14,12 +27,5 @@ export const logger = createLogger({
 		// winston.format.colorize(),
 		winston.format.printf(({ level, message }) => `${level}: ${message}`)
 	),
-	transports: [
-		new winston.transports.Console({
-			stderrLevels: ['error'],
-		}),
-		new winston.transports.File({
-			filename: `data/${new Date(Date.now()).toDateString()}.log`,
-		}),
-	],
+	transports,
 });
