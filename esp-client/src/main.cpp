3851cd917c124d5055030e4dd65a3ae95d0f5500
floergri <148945984+floergri@users.noreply.github.com>
2023-12-01 11:13:31 +0100

    Funktion zum Erfassen des aktuellen Gewichts (#51)

--- esp-client/src/main.cpp
+++ esp-client/src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <HX711.h>
+
+#define threshhold 2.0 // value in grams, above or below which no change will be reported
 
 #define SSID "Postmelder-Wifi"
 IPAddress mqttServer(10, 42, 0, 1);
@@ -10,6 +13,22 @@ IPAddress mqttServer(10, 42, 0, 1);
 WiFiClient wiFiClient;
 PubSubClient client(wiFiClient);
 
+HX711 scale; // scale object
+
+uint8_t dataPin = 32; // scale pins
+uint8_t clockPin = 33;
+
+uint32_t start, stop;
+volatile float f;
+
+float weight = 0; // scale measurement variables
+float previousweight = 0;
+
+bool weightchange = 0; //saves whether weight has changed
+
+float scaleValue; // calibrated scale values -> Flash
+long scaleOffset; // -> Flash
+
 void callback(char *topic, byte *message, unsigned int length);
 void reconnect();
 void sendWeight(float weight);
@@ -17,9 +36,12 @@ const String MAC = WiFi.macAddress();
 bool connectedWithNode = false;
 bool isServerOnline = false;
 
+void calibrateScale();
+float readScale();
+
 void setup()
 {
-	Serial.begin(115200);
+	Serial.begin(115200); // Serial connection to PC
 
 	// Connect to WiFi
 	Serial.print("Connect to: ");
@@ -28,6 +50,15 @@ void setup()
 
 	client.setServer(mqttServer, 1883);
 	client.setCallback(callback);
+
+	scale.begin(dataPin, clockPin); // start scale
+	while (!scale.is_ready())
+	{ // wait until scale started
+		delay(100);
+	}
+
+	calibrateScale(); // only if not initialised
+	weight = readScale();
 }
 
 void loop()
@@ -40,6 +71,38 @@ void loop()
 	{
 		reconnect();
 	}
+
+	if (scale.is_ready())
+	{ // check if scale is ready
+		previousweight = weight;
+		weight = readScale();
+
+		while (weight >= previousweight + threshhold || weight <= previousweight - threshhold)
+		{ // if weight changed above or below threshhold loop until weight has settled in
+			weightchange = true;
+
+			Serial.print("Change detected, weight: "); // print to serial monitor
+			Serial.print(weight, 1);
+			Serial.println("g");
+
+			previousweight = weight;
+			weight = readScale();
+
+			delay(250);
+		}
+
+		if (weightchange)
+		{									// if weight changed over threshhold
+			Serial.print("final weight: "); // print to serial monitor
+			Serial.print(weight);
+			Serial.println("g");
+
+			weightchange = false; // reset change
+
+			sendWeight(weight); // send weight over MQTT
+		}
+	}
+
 	client.loop();
 }
 void callback(char *topic, byte *message, unsigned int length)
@@ -97,4 +160,67 @@ void reconnect()
 void sendWeight(float weight)
 {
 	client.publish(("/" + MAC + "/currentWeight").c_str(), String(weight, 1).c_str(), true);
+}
+
+void calibrateScale()
+{ // calibrates the scale with a user dialog via the serial monitor
+	Serial.println("\n\nCalibration\n===========");
+	Serial.println("remove all weight from the scale");
+	//  flush Serial input
+	while (Serial.available())
+		Serial.read();
+
+	Serial.println("and press enter, 'New Line' has to be activated in the serial monitor"
+				   "\n");
+	while (Serial.available() == 0)
+		;
+
+	Serial.println("calculating scaleOffset");
+	scale.tare(20); // mean of 20 measurements
+	scaleOffset = scale.get_offset();
+
+	Serial.print("scaleOffset: ");
+	Serial.println(scaleOffset);
+	Serial.println();
+
+	Serial.println("place known weight on scale");
+	//  flush Serial input
+	while (Serial.available())
+		Serial.read();
+
+	Serial.println("type in the weight in whole grams and press enter");
+	uint32_t weight = 0;
+	while (Serial.peek() != '\n')
+	{
+		if (Serial.available())
+		{
+			char ch = Serial.read();
+			if (isdigit(ch))
+			{
+				weight *= 10;
+				weight = weight + (ch - '0');
+			}
+		}
+	}
+
+	Serial.print("WEIGHT: ");
+	Serial.println(weight);
+	scale.calibrate_scale(weight, 20);
+
+	scaleValue = scale.get_scale();
+
+	Serial.println(scaleValue, 6);
+	scale.set_offset(scaleOffset); // set scale values
+	scale.set_scale(scaleValue);
+
+	Serial.println("\n\n");
+}
+
+float readScale()
+{ // reads the scale
+	float value = 0;
+
+	value = scale.get_units(20); // mean of 20 measurements
+
+	return value;
 }
