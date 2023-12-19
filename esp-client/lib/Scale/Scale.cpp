b7e3a9d1298648daae20849a880e1944740f7066
binsim <jonasspitzhirn@gmail.com>
2023-12-19 16:09:47 +0100

    Resolve ChangeRequests

--- esp-client/lib/Scale/Scale.cpp
+++ esp-client/lib/Scale/Scale.cpp
@@ -41,7 +41,8 @@ void Scale::init()
 
 float Scale::calibrateScaleOffset()
 {
-	// FIXME: Don't apply changes to scale yet
+	// Changes are directly applied to the scale
+
 	Serial.println("calculating scaleOffset");
 	scale.tare(20); // mean of 20 measurements
 	float offset = scale.get_offset();
@@ -56,7 +57,7 @@ float Scale::calibrateScaleOffset()
 }
 float Scale::calibrateScaleFactor(unsigned int grams)
 {
-	// FIXME: Don't apply changes to scale yet
+	// Changes are directly applied to the scale
 	Serial.println("calculating scale conversion factor...");
 
 	scale.calibrate_scale(grams, 20);
@@ -74,25 +75,42 @@ void Scale::saveScaleValues()
 {
 	this->initialised = this->offset && this->factor;
 
-	// TODO: Apply Scale changes
+	// Changes get applied during the other calibrate steps
+	// These values get saved to the memory
 
 	preferences.begin("postmelder", false); // start preferences
 
-	preferences.putFloat("scaleValue", this->factor); // save values to flash
-	preferences.putLong("scaleOffset", this->offset);
-	preferences.putBool("initialised", this->initialised);
+	// Change values if they have changed
+	if (preferences.getFloat("scaleValue", 0) != this->factor)
+		preferences.putFloat("scaleValue", this->factor);
+	if (preferences.getLong("scaleOffset") != this->offset)
+		preferences.putLong("scaleOffset", this->offset);
+	if (preferences.getBool("initialised") != this->initialised)
+		preferences.putBool("initialised", this->initialised);
 
 	preferences.end(); // close preferences
 }
 void Scale::cancelCalibration()
 {
-	preferences.begin("postmelder", true); // start preferences
+	// Get values from before calibration
+	preferences.begin("postmelder", true);
 
-	this->factor = preferences.getFloat("scaleValue", 0); // read values from flash
+	this->factor = preferences.getFloat("scaleValue", 0);
 	this->offset = preferences.getLong("scaleOffset", 0);
-	this->initialised = preferences.getBool("initialised"); // read flag from flash
+	this->initialised = preferences.getBool("initialised");
 
 	preferences.end();
+
+	if (this->initialised)
+	{
+		// Reset values
+		scale.set_offset(this->offset);
+		scale.set_scale(this->factor);
+	}
+	else
+	{
+		// TODO: Handle case
+	}
 }
 
 float Scale::readWeight()
