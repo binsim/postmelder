89841754824733ba3ac4e18ca6479c9633b8cbd7
binsim <64983699+binsim@users.noreply.github.com>
2024-01-16 12:54:40 +0100

    Schliessen wenn message nicht gesendet (#113)
    * alert und beenden der Funktion, wenn status nicht ok
    
    * Close dialog

--- rpi-server/public/index.js
+++ rpi-server/public/index.js
@@ -204,8 +204,18 @@ async function testMessage(e, deviceId) {
 	rejected_destinations_ul.innerHTML = '';
 
 	// Trigger test message sending and get response
-	const response = await (await fetch('/testMessage?id=' + deviceId)).json();
+	let response = await fetch('/testMessage?id=' + deviceId);
 
+	if (response.status != 200) {
+		// Could not send message, show result
+		alert(await response.text());
+
+		// Close dialog
+		testmessage_response_dialog.close();
+		return;
+	}
+
+	response = await response.json();
 	// Remove loader
 	loader.style.display = 'none';
 
