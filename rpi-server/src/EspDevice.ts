c17e3e078facdb0d8766dc5720487fc5ba8c2442
floergri <floergri@gmail.com>
2024-01-18 11:08:21 +0100

    Kalibrierungs-Timeout erhöht

--- rpi-server/src/EspDevice.ts
+++ rpi-server/src/EspDevice.ts
@@ -376,7 +376,7 @@ export class Device extends EventEmitter implements IDevice {
 				reject(
 					'Timeout for calcScaleOffset elapsed, response took to long'
 				);
-			}, 10_000);
+			}, 15_000);
 
 			MQTTService.Instance.publish(
 				`/${this._device.id}/command/CalcOffset`,
@@ -405,7 +405,7 @@ export class Device extends EventEmitter implements IDevice {
 				reject(
 					'Timeout for calcScaleOffset elapsed, response took to long'
 				);
-			}, 10_000);
+			}, 15_000);
 
 			MQTTService.Instance.publish(
 				`/${this._device.id}/command/CalibrateScale`,
